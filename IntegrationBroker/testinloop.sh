48e6f025e3368bb8ca70a2984ecc62b8cfe1d1ca
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-03-06 09:21:43 +0530

    Update testinloop.sh

--- IntegrationBroker/testinloop.sh
+++ IntegrationBroker/testinloop.sh
@@ -1,2 +1,10 @@
 --BASH
-for i in {1..10}; do curl -X GET -k -H 'Accept: application/json' -u 'iot:welcome1' https://slc16jgp.us.oracle.com/iot/api/v2/monitoring/availability -s -o /dev/null -w '\n==== cURL measurement stats ====\nEstConn: %{time_connect}s\nTimeNameLookup: %{time_namelookup}s\nTimeAppConnect: %{time_appconnect}s\nTimePreTransfer: %{time_pretransfer}s\nTimeStartTransfer: %{time_starttransfer}s\nTotalTime: %{time_total}s\n'; done
+for i in {1..10}; do curl -X GET -k -H 'Accept: application/json' -u 'iot:welcome1' 
+
+https://slc16jgp.us.oracle.com/iot/api/v2/monitoring/availability -s -o /dev/null -w '\n==== cURL 
+
+measurement stats ====\nIteration: '$i'\nEstConn: %{time_connect}s\nTimeNameLookup: 
+
+%{time_namelookup}s\nTimeAppConnect: %{time_appconnect}s\nTimePreTransfer: %{time_pretransfer}s
+
+\nTimeStartTransfer: %{time_starttransfer}s\nTotalTime: %{time_total}s\n'; done
