65f0ae16fe3e8b7542a98050130dcef060c2ed11
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-05-13 14:50:48 +0530

    Update AEPerformanceData.sql
    Added details about build on my local OVA

--- SQLs/AEPerformanceData.sql
+++ SQLs/AEPerformanceData.sql
@@ -1,3 +1,32 @@
+--My Local Build
+SELECT * FROM PS_PP_AE_LOG_TBL ORDER BY PROCESS_INSTANCE DESC;
+
+SELECT * FROM PS_PP_PROJ_RES;
+--TRUNCATE TABLE PS_PP_PROJ_RES REUSE STORAGE;
+SELECT BUSINESS_UNIT, COUNT(*) FROM PS_PROJ_RESOURCE GROUP BY BUSINESS_UNIT;
+--WHERE BUSINESS_UNIT = 'US001';
+
+WITH T1 AS
+(
+SELECT PROCESS_INSTANCE, OPRID, RUNCNTLID, AE_APPLID, AE_SECTION, AE_STEP, DESCR, SQLID, NUMROWS, 
+       TO_DATE(TO_CHAR(STARTDATETIME,'yyyymmddhh24.mi.ss'),'yyyymmddhh24.mi.ss') real_start, 
+       TO_DATE(TO_CHAR(ENDDATETIME,'yyyymmddhh24.mi.ss'),'yyyymmddhh24.mi.ss') real_end,
+       TO_TIMESTAMP(TO_CHAR(STARTDATETIME,'yyyymmddhh24.mi.ssff3'),'yyyymmddhh24.mi.ssff3') new_start, 
+       TO_TIMESTAMP(TO_CHAR(ENDDATETIME,'yyyymmddhh24.mi.ssff3'),'yyyymmddhh24.mi.ssff3') new_end
+FROM PS_PP_AE_LOG_TBL
+)
+SELECT PROCESS_INSTANCE, OPRID, RUNCNTLID, AE_APPLID "App Engine", AE_SECTION "Section", AE_STEP "Step", DESCR, SQLID, NUMROWS "Rows(s) Affected", --new_start, new_end,
+       CASE 
+           WHEN trunc(24*mod(real_end - real_start,1)) < 0
+           THEN 86400 + trunc(mod(real_end - real_start,1)*24*60*60,1) --assumption is that its one day
+           ELSE trunc(mod(real_end - real_start,1)*24*60*60,1)
+       END AS TOTAL_SECONDS, extract(second from new_end - new_start) * 1000 "MILLISECONDS",
+       trunc(real_end - real_start) days, trunc(24*mod(real_end - real_start,1)) as hrs, trunc(mod(mod(real_end - real_start,1)*24,1)*60 ) as mins, 
+       trunc(mod(mod(mod(real_end - real_start,1)*24,1)*60, 1)*60) as seconds
+FROM T1 
+ORDER BY PROCESS_INSTANCE DESC;
+
+--8.58_134W Build
 --Version 1
 WITH T1 AS
 (
