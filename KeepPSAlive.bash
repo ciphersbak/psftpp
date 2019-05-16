a5e412064d427e6d85afb5cb3f22918c0019c37a
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-05-16 21:47:01 +0530

    parametrise the script

--- KeepPSAlive.bash
+++ KeepPSAlive.bash
@@ -1,3 +1,8 @@
 --BASH
 #!/bin/bash
 for i in {1..10}; do curl -XGET -k "http://slc08vkx.us.oracle.com:8000/psp/e92pp858x/?cmd=login&languageCd=ENG&" -s -o /dev/null --user VP1:VP1; done
+
+--BASH
+#!/bin/bash
+$ proto="http" portNum="8000" wsName="slc08vkx" siteName="e92pp858" userName="VP2";
+curl -X GET -k $proto"://"$wsName".us.oracle.com:"$portNum"/psp/"$siteName"x/?cmd=login&languageCd=ENG&" -v -s -o /dev/null --user $userName:$userName;
