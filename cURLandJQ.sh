3063abd837744b5e0716c6d021a26e6c632f15c3
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-05-22 14:34:47 +0530

    Update cURLandJQ.sh

--- cURLandJQ.sh
+++ cURLandJQ.sh
@@ -7,4 +7,4 @@ curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP
  
 curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '.Get__CompIntfc_PP_WEB_PROF_HIST_CIResponse.WEBPROFILENAME, .Get__CompIntfc_PP_WEB_PROF_HIST_CIResponse.WEBSITENAME'
  
-curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '{webprofileName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBSITENAME, websiteName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBSITENAME}'
+curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '{webprofileName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBPROFILENAME, websiteName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBSITENAME}'
