108583034ea110debc4b7a7a85f9ccf976f30adc
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-05-22 14:20:31 +0530

    JQ Plugin for cURL
    Link to Tutorial - https://stedolan.github.io/jq/tutorial/

---
+++ cURLandJQ.sh
@@ -0,0 +1,10 @@
+alias jq=./jq-win64.exe
+echo '{"foo": 0}' | jq
+ 
+curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq
+ 
+curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '.Get__CompIntfc_PP_WEB_PROF_HIST_CIResponse.WEBPROFILENAME'
+ 
+curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '.Get__CompIntfc_PP_WEB_PROF_HIST_CIResponse.WEBPROFILENAME, .Get__CompIntfc_PP_WEB_PROF_HIST_CIResponse.WEBSITENAME'
+ 
+curl -XGET http://slc11ohu.us.oracle.com:8000/PSIGW/RESTListeningConnector/E92PP858/CIRT_PP_WEB_PROF_HIST_C_G_GET.V1/slc11ohu/8000/e92pp858x -H "Accept: application/json" -s | jq '{webprofileName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBSITENAME, websiteName: .Get__CompIntfc__PP_WEB_PROF_HIST_CIResponse.WEBSITENAME}'
