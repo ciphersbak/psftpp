0b2a10890ba2600e9446c1f8898980a6b0de3591
Prashant Prakash <oracle.prashantprakash@gmail.com>
2019-08-23 07:58:17 +0530

    Delete testping.sh

--- IntegrationBroker/testping.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET -k -H 'Accept: application/json' -u 'iot:welcome1' https://slc16jgp.us.oracle.c
-om/iot/api/v2/monitoring/availability -w '\n\n==== cURL measurement stats ==== \nEstConn: %{time_connect}s\nTimeNameLook
-up: %{time_namelookup}s\nTimeAppConnect: %{time_appconnect}s\nTimePreTransfer: %{time_pretransfer}s\nTimeStartTransfer:
-%{time_starttransfer}s\nTotalTime: %{time_total}s\n' -s -o /dev/null
